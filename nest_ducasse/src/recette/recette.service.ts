fca6d2a5db94c32060b50f07c34839b8b245369f
ThomasH <thochet.web@gmail.com>
2023-04-27 10:22:54 +0200

    Formatage Appel recettes

--- nest_ducasse/src/recette/recette.service.ts
+++ nest_ducasse/src/recette/recette.service.ts
@@ -20,20 +20,75 @@ export class RecetteService {
   ) {}
 
   async findAll() {
-    return await this.recetteRepository.createQueryBuilder('recette')
-                                       .leftJoinAndSelect('recette.ingredientToRecette', 'rti')
-                                       .leftJoinAndSelect('rti.ingredient', 'ingredient')
-                                       .leftJoinAndSelect('recette.etapes', 'etape')
-                                       .getMany();
+    // return await this.recetteRepository.createQueryBuilder('recette')
+    //                                    .leftJoinAndSelect('recette.ingredientToRecette', 'rti')
+    //                                    .leftJoinAndSelect('rti.ingredient', 'ingredient')
+    //                                    .leftJoinAndSelect('recette.etapes', 'etape')
+    //                                    .getMany();
+    const queryBuilder = this.recetteRepository.createQueryBuilder('recette')
+      .leftJoinAndSelect('recette.ingredientToRecette', 'rti')
+      .leftJoinAndSelect('rti.ingredient', 'ingredient')
+      .leftJoinAndSelect('recette.etapes', 'etape');
+
+    const recettes = await queryBuilder.getMany();
+
+    const result = recettes.map((recette) => ({
+      id: recette.id,
+      nom: recette.nom,
+      description: recette.description,
+      photo: recette.photo,
+      temps_preparation: recette.temps_preparation,
+      temps_cuisson: recette.temps_cuisson,
+      date_ajout: recette.date_ajout,
+      ingredients: recette.ingredientToRecette.map((rti) => ({
+        id: rti.ingredient.id,
+        nom: rti.ingredient.nom,
+        photo: rti.ingredient.photo,
+        allergene: rti.ingredient.allergene,
+        dosage: rti.dosage
+      })),
+      etapes: recette.etapes.map((etape) => ({
+        id: etape.id,
+        numero_ordre: etape.numero_ordre,
+        instructions: etape.instructions
+      })),
+    }));
+
+    return result;   
   }
 
   async findOne(id: number) {
-    return await this.recetteRepository.createQueryBuilder('recette')
-                                       .leftJoinAndSelect('recette.ingredientToRecette', 'rti')
-                                       .leftJoinAndSelect('rti.ingredient', 'ingredient')
-                                       .leftJoinAndSelect('recette.etapes', 'etape')
-                                       .where('recette.id = :id', { id: id })
-                                       .getOne();
+    const queryBuilder = this.recetteRepository.createQueryBuilder('recette')
+      .leftJoinAndSelect('recette.ingredientToRecette', 'rti')
+      .leftJoinAndSelect('rti.ingredient', 'ingredient')
+      .leftJoinAndSelect('recette.etapes', 'etape')
+      .where('recette.id = :id', { id: id });
+
+    const recette = await queryBuilder.getOne();
+
+    const result = {
+      id: recette.id,
+      nom: recette.nom,
+      description: recette.description,
+      photo: recette.photo,
+      temps_preparation: recette.temps_preparation,
+      temps_cuisson: recette.temps_cuisson,
+      date_ajout: recette.date_ajout,
+      ingredients: recette.ingredientToRecette.map((rti) => ({
+        id: rti.ingredient.id,
+        nom: rti.ingredient.nom,
+        photo: rti.ingredient.photo,
+        allergene: rti.ingredient.allergene,
+        dosage: rti.dosage
+      })),
+      etapes: recette.etapes.map((etape) => ({
+        id: etape.id,
+        numero_ordre: etape.numero_ordre,
+        instructions: etape.instructions
+      })),
+    };
+
+    return result;
   }
 
   update(id: number, recette: Recette) {
